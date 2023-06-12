82a320d544a45a9a62b512e60d70987e8eed7e69
loadingthedev <loadingthedev@gmail.com>
2023-06-12 16:40:49 +0530

    revert changes

--- src/components/Schema.jsx
+++ src/components/Schema.jsx
@@ -3,9 +3,10 @@ import Head from "next/head";
 export default function Schema({ navigation, siteLink, faq }) {
   return (
     <Head>
-      <script type="application/ld+json">{navigation}</script>
-      {/* <script type="application/ld+json">{JSON.stringify(siteLink)}</script>
-      <script type="application/ld+json">{JSON.stringify(faq)}</script> */}
+      <script type="application/ld+json">{JSON.stringify(navigation)}</script>
+      <script type="application/ld+json">{JSON.stringify(siteLink)}</script>
+      <script type="application/ld+json">{JSON.stringify(faq)}</script>
     </Head>
   );
 }
+ 
